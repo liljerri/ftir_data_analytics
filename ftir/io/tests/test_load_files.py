e5aa2af5780441e71b0a959875a6fc55ac5cec86
liljerri <jyoung@kbibiopharma.com>
2019-01-08 17:49:15 -0700

    add test file

---
+++ ftir/io/tests/test_load_files.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from ftir.io.utils import create_df_from_multiple_files, \
+    create_df_from_single_file
+
+
+class TestSingleFile(TestCase):
+    def setUp(self):
+        pass
+
+
+class TestMultipleFiles(TestCase):
+    def setUp(self):
+        pass
